64f604f9c567018c39461c3c61d0ce5de1f1e04d
Diego Sperandio <godie.sperandio@gmail.com>
2024-08-08 22:57:00 -0300

    Adicionado exercicios faltantes

--- RocketseatC#Fundamentos/Fundamentos/Program.cs
+++ RocketseatC#Fundamentos/Fundamentos/Program.cs
@@ -1,4 +1,6 @@
-﻿bool exec = true;
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+bool exec = true;
 
 while (exec)
 {
@@ -6,7 +8,10 @@ while (exec)
     Console.WriteLine("1 - Nome e mensagem personalizada");
     Console.WriteLine("2 - Nome e Sobrenome");
     Console.WriteLine("3 - Soma, subtração, multiplicação, divisão e média ");
-    Console.WriteLine("5 - Sair");
+    Console.WriteLine("4 - Retornar quantidade de caracter");
+    Console.WriteLine("5 - Retornar se a placa é valida");
+    Console.WriteLine("6 - Exibir data em diferentes formatos");
+    Console.WriteLine("7 - Sair");
     Console.WriteLine("-------------------------------------");
 
     Console.Write("\nInforme o código da opção desejada: ");
@@ -29,7 +34,22 @@ while (exec)
                 Operations();
             }
             break;
+        case "4":
+            {
+                NumberOfCharacters();
+            }
+            break;
         case "5":
+            {
+                ValidatePlate();
+            }
+            break;
+        case "6":
+            {
+                ShowDate();
+            }
+            break;
+        case "7":
             {
                 exec = false;
                 Console.WriteLine("Volte sempre!!");
@@ -99,20 +119,39 @@ while (exec)
     static void ReportNumbers()
     {
         Console.Write("\nDigite o primeiro numero: ");
-        var numberOne = Convert.ToDouble(Console.ReadLine());
+        double numberOne = Convert.ToDouble(Console.ReadLine());
 
         Console.Write("Digite o segundo numero: ");
-        var numberTwo = Convert.ToDouble(Console.ReadLine());
+        double numberTwo = Convert.ToDouble(Console.ReadLine());
 
-        var sum = numberOne + numberTwo;
-        var multiplication = numberOne * numberTwo;
-        var division = numberOne / numberTwo;
-        var average = (numberOne + numberTwo) / 2;
+        double sum = numberOne + numberTwo;
+        double subtraction = numberOne - numberTwo;
+        double multiplication = numberOne * numberTwo;
+
+        string error = string.Empty;
+        double division = 0;
+
+        if (numberTwo == 0)
+        {
+            error = "Não é possível dividir por zero";
+        }
+        else
+        {
+            division = numberOne / numberTwo;
+        }
+
+        double average = (numberOne + numberTwo) / 2;       
 
         Console.WriteLine("");
         Console.WriteLine("Soma: {0} + {1} = {2}", FormatNumber(numberOne), FormatNumber(numberTwo), FormatNumber(sum));
+        Console.WriteLine("Subtração: {0} - {1} = {2}", FormatNumber(numberOne), FormatNumber(numberTwo), FormatNumber(subtraction));
         Console.WriteLine("Multiplicação: {0} * {1} = {2}", FormatNumber(numberOne), FormatNumber(numberTwo), FormatNumber(multiplication));
-        Console.WriteLine("Divisão: {0} / {1} = {2}", FormatNumber(numberOne), FormatNumber(numberTwo), FormatNumber(division));
+
+        if (!string.IsNullOrEmpty(error))
+            Console.WriteLine("Divisão: {0} / {1} = {2}", FormatNumber(numberOne), FormatNumber(numberTwo), error);
+        else
+            Console.WriteLine("Divisão: {0} / {1} = {2}", FormatNumber(numberOne), FormatNumber(numberTwo), FormatNumber(division));
+
         Console.WriteLine("Média: ({0} + {1}) / 2 = {2}", FormatNumber(numberOne), FormatNumber(numberTwo), FormatNumber(average));
         Console.WriteLine("");
         Console.WriteLine("Precione qualquer tecla para voltar para o menu.");
@@ -127,14 +166,33 @@ while (exec)
         double numberTwo = random.Next(1, 1000);
 
         double sum = numberOne + numberTwo;
+        double subtraction = numberOne - numberTwo;
         double multiplication = numberOne * numberTwo;
-        double division = numberOne / numberTwo;
+
+        string error = string.Empty;
+        double division = 0;
+
+        if (numberTwo == 0)
+        {
+            error = "Não é possível dividir por zero";
+        }
+        else
+        {
+            division = numberOne / numberTwo;
+        }
+
         double average = (numberOne + numberTwo) / 2;
 
         Console.WriteLine("");
         Console.WriteLine("Soma: {0} + {1} = {2}", FormatNumber(numberOne), FormatNumber(numberTwo), FormatNumber(sum));
+        Console.WriteLine("Subtração: {0} - {1} = {2}", FormatNumber(numberOne), FormatNumber(numberTwo), FormatNumber(subtraction));
         Console.WriteLine("Multiplicação: {0} * {1} = {2}", FormatNumber(numberOne), FormatNumber(numberTwo), FormatNumber(multiplication));
-        Console.WriteLine("Divisão: {0} / {1} = {2}", FormatNumber(numberOne), FormatNumber(numberTwo), FormatNumber(division));
+
+        if (!string.IsNullOrEmpty(error))
+            Console.WriteLine("Divisão: {0} / {1} = {2}", FormatNumber(numberOne), FormatNumber(numberTwo), error);
+        else
+            Console.WriteLine("Divisão: {0} / {1} = {2}", FormatNumber(numberOne), FormatNumber(numberTwo), FormatNumber(division));
+
         Console.WriteLine("Média: ({0} + {1}) / 2 = {2}", FormatNumber(numberOne), FormatNumber(numberTwo), FormatNumber(average));
         Console.WriteLine("");
         Console.WriteLine("Precione qualquer tecla para voltar para o menu.");
@@ -147,5 +205,82 @@ while (exec)
             return ((int)number).ToString("N0");
         else
             return number.ToString("F2");
+    }
+
+    static void NumberOfCharacters()
+    {
+        Console.Write("\nDigite uma ou mais palavras (separadas por espaços): ");
+        string input = Console.ReadLine();
+        Console.WriteLine("");
+
+        string[] palavras = input.Split(" ");
+
+        foreach (string palavra in palavras)
+        {
+            if (!string.IsNullOrEmpty(palavra))
+            {
+                Console.WriteLine($"A palavra '{palavra}' tem {palavra.Length} caracteres.");
+            }
+        }
+    }
+
+    static void ValidatePlate()
+    {
+        Console.Write("\nDigite o numero da placa: ");
+        string plate = Console.ReadLine();
+        Console.WriteLine("");
+
+        if (!string.IsNullOrEmpty(plate) && plate.Length == 7)
+        {
+            if (IsValidPlate(plate))
+            {
+                Console.WriteLine("Verdadeiro, a placa é valida!");
+            }
+            else
+            {
+                Console.WriteLine("Falso, a placa é invalida!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Falso, a placa é invalida!");
+        }
+    }
+
+    static bool IsValidPlate(string plate)
+    {
+        bool isValid = true;
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (!char.IsLetter(plate[i]))
+            {
+                isValid = false;
+                return false;
+            }
+        }
+
+        for (int i = 3; i < 7; i++)
+        {
+            if (!char.IsDigit(plate[i]))
+            {
+                isValid = false;
+                return false;
+            }
+        }
+
+        return isValid;
+    }
+
+    static void ShowDate()
+    {
+        Console.Write("\nFormato completo: ");
+        Console.WriteLine(DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
+        Console.Write("Apenas a data: ");
+        Console.WriteLine(DateTime.Now.ToString("dd/MM/yyyy"));
+        Console.Write("Apenas a hora: ");
+        Console.WriteLine(DateTime.Now.ToString("HH:mm"));
+        Console.Write("Data com o mês por extenso: ");
+        Console.WriteLine(DateTime.Now.ToString("dd 'de' MMMM 'de' yyyy"));
     }
 }
